1c189fd51c5e92b811fe8c00846b15f3143e216c
vchacin <arq.vchacin@gmail.com>
2021-02-17 09:01:19 -0300

    -Responsive design -Animaciones iniciales

---
+++ scripts/lang.js
@@ -0,0 +1,89 @@
+const LANG = {
+    NAVBAR: {
+        EN: "Deliver with Uber"
+    },
+    HEADER: {
+        TEXTO1: {
+            PR: "Faça entregas com " ,
+            EN: "Make deliveries with "
+        },
+        TEXTO2: {
+            PR: "Ganhe dinheiro de acordo com sua programação" ,
+            EN: "Earn money with your own schedule"
+        }
+    },
+    MAIN: {
+        TEXT1:{
+            PR: "Entregue quando quiser",
+            EN: "Deliver when you want"
+        },
+        TEXT2:{
+            PR: "Trabalhe de acordo com sua programação",
+            EN: "Work on your own schedule"
+        },
+        TEXT3:{
+            PR: "Distribua algumas horas pela manhã, todas as noites ou apenas nos fins de semana. Você decide. Você é seu próprio patrão e pode escolher quando e quanto trabalhar.",
+            EN: "Spread a few hours in the morning, every night or just on weekends. You decide. You are your own boss and you can choose when and how much you work."
+        },
+        TEXT4:{
+            PR: "Entregue com o Uber",
+            EN: "Deliver with Uber"
+        },
+        TEXT5:{
+            PR: "Escolha o seu veículo",
+            EN: "Choose your vehicle"
+        },
+        TEXT6:{
+            PR: "Dependendo da sua cidade, você pode fazer entregas com seu veículo, bicicleta ou scooter.",
+            EN: "Depending on your city, you can make deliveries with your vehicle, bicycle or scooter."
+        },
+        TEXT7:{
+            PR: "Entregue com o Uber",
+            EN: "Deliver with Uber"
+        },
+        TEXT8:{
+            PR: "Ganhar dinheiro" ,
+            EN: "Earn money"
+        },
+        TEXT9:{
+            PR: "Você ganhará dinheiro levando às pessoas o que elas mais gostam. O que você faz entre uma entrega e outra é com você.",
+            EN: "You will earn money by bringing people what they like the most. What you do between one delivery and another is up to you."
+        },
+        TEXT10:{
+            PR: "Entregue com o Uber",
+            EN: "Deliver with Uber"
+        }
+    },
+    FOOTER:{
+        TEXT1: {
+            PR: "Termos e Condições",
+            EN: "Terms and Conditions"
+        }
+    },
+    LEGAL:{
+        HEADER:{
+            TEXT1C: {
+                PR: "Última modificação: 09/02/2021",
+                EN: "Terms"
+            },
+            TEXT2C: {
+                PR: "Termos e Condições",
+                EN: "Terms and Conditions"
+            },
+            TEXT3C: {
+                PR: "Última atualização: 9 de fevereiro de 2021",
+                EN: "Last update: February 9 2021"
+            }
+        },
+        MAIN:{
+            TEXT1M: {
+                PR: "O usuário entende que a utilização dos NOSSOS SERVIÇOS DE REGISTRO INDIRETO COM A EMPRESA, SERVEM TOTALMENTE E APENAS PODEM CONECTAR CONDUTORES A PLATAFORMAS DE EMPREGO, para prosperar e aproximar os prestadores de serviços dos serviços em geral. Após o preenchimento de sua inscrição para admissão, cada parceiro de motorista deve enviar seus requisitos para os portais digitais oficiais, que serão redirecionados por nós, porém esses extremos ajudarão a completar o processo de incorporação do trabalho.",
+                EN: "The user understands that the use of OUR INDIRECT REGISTRATION SERVICES WITH THE COMPANY, SERVE TOTALLY AND ONLY TO BE ABLE TO CONNECT DRIVING PARTNERS WITH EMPLOYMENT PLATFORMS, to prosper and bring service providers closer to services in general. After you have completed your application for admission, each driving partner must submit their requirements to the official digital portals, which will be redirected by us, however these ends will help to complete the job incorporation process."
+            },
+            TEXT2M: {
+                PR: "Partners Drivers, atua como um intermediário oficial com licenças de promoção direta para afiliação de motoristas parceiros na América Central, América Latina e partes do Leste Asiático.",
+                EN: "Partners Drivers, acts as an official intermediary with direct promotion permits for affiliation of driver partners in Central America, Latin America and parts of East Asia."
+            }
+        }
+    }
+}
