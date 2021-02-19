a2ac7feb63ab9ce64e1463a3e856610087e99d4b
vchacin <arq.vchacin@gmail.com>
2021-02-19 07:39:23 -0300

    -Imagenes optimizadas -Animaciones -Pulse del map -Falta navbar

---
+++ scripts/animar.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', function(){
+
+    function aparece (direccion){
+    
+        let animado = document.querySelectorAll(".anima_" + direccion);
+        let scrollTop = document.documentElement.scrollTop;
+
+        for (var i =0; i<animado.length; i++) {
+            let alturaAnimado = animado[i].offsetTop;
+            if (alturaAnimado - 300 < scrollTop) {
+                animado[i].style.opacity = 1;
+                animado[i].classList.add("mostrar_" + direccion);
+            }
+         }
+    }
+
+aparece("up");
+aparece("right");
+
+
+});
