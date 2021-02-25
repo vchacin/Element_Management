bc88ee140b0612fb06586b71c08808756a6a5317
vchacin <arq.vchacin@gmail.com>
2021-02-25 19:24:39 -0300

    -"topscreen actualizado, reduccion de tiempo de animacion"

--- scripts/animar.js
+++ scripts/animar.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', function(){
 
         for (var i =0; i<animado.length; i++) {
             let alturaAnimado = animado[i].offsetTop;
-            if (alturaAnimado - 300 < scrollTop) {
+            if (alturaAnimado - 400 < scrollTop) {
                 animado[i].style.opacity = 1;
                 animado[i].classList.add("mostrar_" + direccion);
             }
