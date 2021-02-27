1846dcd0e1ee708a7dd1cde0595741916290f64e
vchacin <arq.vchacin@gmail.com>
2021-02-27 14:34:26 -0300

    -Menu flotante -Feature de traduccion -Justificacion de textos en Mision/Vision -Form actualizado -Fontawesome de facebook actualizado -Formato de sms de whatsapp con estructura -Icon de whatsapp ajustado flotante -h4 en location de riviera Maya

--- scripts/lang.js
+++ scripts/lang.js
@@ -2,23 +2,29 @@ const LANG = {
     HEADER: {
         NAVBAR: {
             TEXTO1: {
+                ES: "Inicio",
                 EN: "Home"
             },
             TEXTO2: {
+                ES: "Nosotros",
                 EN: "About Us"
             },
             TEXTO3: {
+                ES: "Servicios", 
                 EN: "Services"
             },
             TEXTO4: {
+                ES: "Ubicación",
                 EN: "Location"
             },
             TEXTO5: {
+                ES: "Contacto",
                 EN: "Contact"
             }
         },
         DIV: {
             TEXTO1: {
+                ES: "Cuidamos cada elemento <br/> de tu patrimonio",
                 EN: "We take care of each element <br/> of your heritage"
             }
         }
@@ -26,204 +32,265 @@ const LANG = {
     MAIN: {
         NOSOTROS: {
             TEXT1:{
+                ES: "SOBRE NOSOTROS",
                 EN: "ABOUT US"
             },
             TEXT2:{
+                ES: "SOMOS UNA EMPRESA ESPECIALIZADA EN LA <span class='font-bold'>ADMINISTRACIÓN PROFESIONAL E INTEGRAL DE CONDOMINIOS</span> EN LA RIVIERA MAYA; FOCALIZANDO ESFUERZOS PARA MANTENER EN ÓPTIMAS CONDICIONES LAS ÁREAS COMUNES Y AMENIDADES DE LOS MISMOS, LOGRANDO EL BUEN FUNCIONAMIENTO DE CADA UNA DE LAS INSTALACIONES DE SU PROPIEDAD.",
                 EN: "WE ARE A COMPANY SPECIALIZED IN THE <span class='font-bold'> PROFESSIONAL AND INTEGRAL CONDO MANAGEMENT</span> ON THE RIVIERA MAYA; FOCUSING EFFORTS TO KEEP COMMON AREAS AND AMENITIES THEREOF IN OPTIMAL CONDITIONS, ACHIEVING THE GOOD OPERATION OF EACH OF THE FACILITIES OF YOUR PROPERTY."
             }
         },
         QUOTE: {
             TEXT1:{
+                ES: "UNA BUENA ADMINISTRACIÓN ES IGUAL A <span class='font-semibold'>UN INMUEBLE EN OPTIMAS CONDICIONES CON ALTA PLUSVALÍA</span>",
                 EN: "A GOOD MANAGEMENT IS EQUAL TO <span class = 'font-semibold'> A PROPERTY IN OPTIMAL CONDITIONS WITH HIGH SURPLUS VALUE </span>"
-            }
+            } 
         },
         SERVICIOS: {
             TEXT1:{
+                ES: "SERVICIOS",
                 EN: "SERVICES"
             },
             TEXT2:{
+                ES: "EXPERTOS EN ADMINISTRACIÓN",
                 EN: "EXPERTS IN ADMINISTRATION"
             },
             TEXT3:{
+                ES: "Talento y capital humano",
                 EN: "Talent and human capital"
             },
             TEXT4:{
+                ES: "Gestión económica eficaz",
                 EN: "Effective financial management"
             },
             TEXT5:{
+                ES: "Generación de valor y calidad",
                 EN: "Generation of value and quality"
             },
             TEXT6:{
+                ES: "Planificación, dirección y control",                
                 EN: "Planning, direction and control"
             },
             TEXT7:{
+                ES: "TECNOLOGÍA AVANZADA",                
                 EN: "ADVANCED TECHNOLOGY"
             },
             TEXT8:{
+                ES: "Procesos eficientes",                
                 EN: "Efficient processes"
             },
             TEXT9:{
+                ES: "Seguridad y transparencia",                
                 EN: "Security and transparency"
             },
             TEXT10:{
+                ES: "Técnicas innovadoras",
                 EN: "Innovative techniques"
             },
             TEXT11:{
+                ES: "Buen manejo de ingresos",
                 EN: "Good income management"
             },
             TEXT12:{
+                ES: "SOPORTE 24/7",
                 EN: "24/7 SUPPORT"
             },
             TEXT13:{
+                ES: "Atención personalizada",
                 EN: "Personalized attention"
             },
             TEXT14:{
+                ES: "Solucionamos problemas",
                 EN: "We solve daily problems of the type"
             },
             TEXT15:{
-                EN: "Operative"
+                ES: "<i class='fas fa-check px-2'></i>Operativos",
+                EN: "<i class='fas fa-check px-2'></i>Operatives"
             },
             TEXT16:{
-                EN: "Administrative"
+                ES: "<i class='fas fa-check px-2'></i>Administrativos",
+                EN: "<i class='fas fa-check px-2'></i>Administratives"
             },
             TEXT17:{
-                EN: "Conflicts"
+                ES: "<i class='fas fa-check px-2'></i>Conflictos",
+                EN: "<i class='fas fa-check px-2'></i>Conflicts"
             },
             TEXT18:{
-                EN: "Information dissemination"
+                ES: "<i class='fas fa-check px-2'></i>Difusión de Información",
+                EN: "<i class='fas fa-check px-2'></i>Information dissemination"
             },
             TEXT19:{
-                EN: "Property Supervision"
+                ES: "<i class='fas fa-check px-2'></i>Supervisión del Inmueble",
+                EN: "<i class='fas fa-check px-2'></i>Property Supervision"
             },
             TEXT20:{
-                EN: "Component Repair"
+                ES: "<i class='fas fa-check px-2'></i>Reparación de Componentes",
+                EN: "<i class='fas fa-check px-2'></i>Component Repair"
             },
             TEXT21:{
+                ES: "PROVEEDORES EXPERTOS",
                 EN: "EXPERT SUPPLIERS"
             },
             TEXT22:{
+                ES: "Trabajo en equipo",
                 EN: "Teamwork"
             },
             TEXT23:{
+                ES: "Alianzas profesionales",
                 EN: "Professional alliances"
             },
             TEXT24:{
+                ES: "Talento local",
                 EN: "Local talent"
             }
         },
         MISION: {
             TEXT1:{
+                ES: "MISIÓN",
                 EN: "MISSION"
             },
             TEXT2:{
+                ES: "SER LA EMPRESA LÍDER DE LA REGIÓN, RECONOCIDA POR NUESTRO SERVICIO DE ALTA CALIDAD Y TECNOLOGÍA APLICADA AL DESARROLLO COTIDIANO DE LA PROPIEDAD CON SEGUIMIENTO PERSONALIZADO DE CADA PROPIETARIO O ADMINISTRADOR, ASEGURANDO SU PATRIMONIO Y SATISFACCIÓN GLOBAL.",
                 EN: "TO BE THE LEADING COMPANY IN THE REGION, RECOGNIZED FOR OUR HIGH QUALITY SERVICE AND TECHNOLOGY APPLIED TO THE DAILY DEVELOPMENT OF THE PROPERTY WITH PERSONALIZED MONITORING OF EACH OWNER OR ADMINISTRATOR, ASSURING THEIR ASSETS AND GLOBAL SATISFACTION."
             },
             TEXT3:{
+                ES: "GUIADA POR LA TRANSPARENCIA, INTEGRIDAD, SOLIDARIDAD, RESPONSABILIDAD Y SEGURIDAD SOCIAL.",
                 EN: "GUIDED BY TRANSPARENCY, INTEGRITY, SOLIDARITY, RESPONSIBILITY AND SOCIAL SECURITY."
             },
             TEXT4:{
+                ES: "VISIÓN",
                 EN: "VISION"
             },
             TEXT5:{
+                ES: "LIDERAR EL MERCADO DE LA ADMINISTRACIÓN CONDO-HOTELERA EN LA REGIÓN POR NUESTROS PROCESOS TECNOLÓGICOS, TRANSPARENTES Y EFECTIVOS.",
                 EN: "LEADING THE CONDO-HOTEL MANAGEMENT MARKET IN THE REGION BY OUR TECHNOLOGICAL, TRANSPARENT AND EFFECTIVE PROCESSES."
             },
             TEXT6:{
+                ES: "GARANTIZANDO UN SERVICIO ÓPTIMO EN CADA ELEMENTO DEL INMUEBLE DE NUESTROS CLIENTES.",
                 EN: "GUARANTEEING AN OPTIMAL SERVICE IN EACH ELEMENT OF THE PROPERTY OF OUR CLIENTS."
             }
         },
         ESQUEMA: {
             TEXT1:{
+                ES: "NUESTRO MÉTODO",
                 EN: "OUR METHOD"
             },
             TEXT2:{
+                ES: "ANÁLISIS",
                 EN: "ANALYSIS"
             },
             TEXT3:{
+                ES: "NECESIDADES, FORTALEZAS Y OPORTUNIDADES.",
                 EN: "NEEDS, STRENGTHS AND OPPORTUNITIES."
             },
             TEXT4:{
+                ES: "PROPUESTA",
                 EN: "PROPOSAL"
             },
             TEXT5:{
+                ES: '“NO SON SOLO NÚMEROS, HAY QUE DARLES VALOR”.',
                 EN: '“THEY ARE NOT JUST NUMBERS, YOU HAVE TO GIVE THEM VALUE”.'
             },
             TEXT6:{
+                ES: "APLICACIÓN",
                 EN: "APPLICATION"
             },
             TEXT7:{
+                ES: "PLAN DE ACCIÓN ACORDE AL CONDOMINIO.",
                 EN: "ACTION PLAN ACCORDING TO THE CONDOMINIUM."
             },
             TEXT8:{
+                ES: "RESIDENTES",
                 EN: "RESIDENTS"
             },
             TEXT9:{
+                ES: "BUENA COMUNICACIÓN Y SEGUIMIENTO.",
                 EN: "GOOD COMMUNICATION AND FOLLOW-UP."
             }
         },
         BENEFICIOS: {
-            TEXTO1: {
+            TEXT1: {
+                ES: "BENEFICIOS",
                 EN: "BENEFITS"
             },
-            TEXTO2: {
+            TEXT2: {
+                ES: "SOLUCIONES INTEGRALES",
                 EN: "INTEGRAL SOLUTIONS"
             },
-            TEXTO3: {
+            TEXT3: {
+                ES: "ACCESO A LA INFORMACIÓN EN TIEMPO REAL Y SOLUCIÓN DE PROBLEMAS INMEDIATOS.",
                 EN: "ACCESS TO REAL-TIME INFORMATION AND IMMEDIATE PROBLEM SOLUTION."
             },
-            TEXTO4: {
+            TEXT4: {
+                ES: "CONTACTO Y ASESORIA",
                 EN: "CONTACT AND ADVICE"
             },
-            TEXTO5: {
+            TEXT5: {
+                ES: "NÚMEROS DE CONTACTO DIRECTO A SU DISPOSICIÓN PARA EMERGENCIAS Y ASESORÍA LEGAL.",
                 EN: "DIRECT CONTACT NUMBERS AT YOUR DISPOSAL FOR EMERGENCIES AND LEGAL ADVICE."
             },
-            TEXTO6: {
+            TEXT6: {
+                ES: "ÓPTIMAS CONDICIONES",
                 EN: "OPTIMAL CONDITIONS"
             },
-            TEXTO7: {
+            TEXT7: {
+                ES: "ENFOCADOS EN MANTENER EN ÓPTIMAS CONDICIONES LAS ÁREAS COMUNES Y AMENIDADES.",
                 EN: "FOCUSED ON KEEPING COMMON AREAS AND AMENITIES IN OPTIMAL CONDITION."
             }
         },
         UBICACION: {
             TEXT1:{
+                ES: "UBICACIÓN",
                 EN: "LOCATION"
             },
             TEXT2:{
+                ES: "ESTAMOS EN LA <span class='font-bold'>RIVIERA MAYA, MÉXICO</span>",
                 EN: "WE ARE IN THE <span class='font-bold'> RIVIERA MAYA, MEXICO </span>"
             }
         },
         CONTACT: {
             TEXT1:{
+                ES: "¿LISTO PARA EMPEZAR? <br/> LLÁMANOS Y MEJORA LA CALIDAD DE VIDA EN TU CONDOMINIO.",
                 EN: "READY TO START? <br/> CALL US AND IMPROVE THE QUALITY OF LIFE IN YOUR CONDOMINIUM."
             },
             TEXT2:{
+                ES: "<label class='tracking-wide text-xs font-bold mb-2' for='name'>NOMBRE</label><input class='w-full bg-gray-200 border border-gray-200 px-4 py-2 mb-2' id='name' type='text' placeholder='Ingresa tu nombre'>",
                 EN: "<label class='tracking-wide text-xs font-bold mb-2' for='name'>NAME</label><input class='w-full bg-gray-200 border border-gray-200 px-4 py-2 mb-2' id='name' type='text' placeholder='Please enter your name'>"
             },
             TEXT3:{
+                ES: "TELÉFONO",
                 EN: "PHONE"
             },
             TEXT4:{
+                ES: "<label class='tracking-wide text-xs font-bold mb-2' for='email'>EMAIL</label><input class='w-full bg-gray-200 border border-gray-200 px-4 py-2' id='email' type='email' placeholder='Ingresa tu email'>",
                 EN: "<label class='tracking-wide text-xs font-bold mb-2' for='email'>EMAIL</label><input class='w-full bg-gray-200 border border-gray-200 px-4 py-2' id='email' type='email' placeholder='Please enter your email'>"
             },
             TEXT5:{
+                ES: "UNIDADES HABITACIONALES",
                 EN: "HOUSING UNITS"
             },
             TEXT6:{
+                ES: "30 o más",
                 EN: "30 or more"
             },
             TEXT7:{
+                ES: "CIUDAD",
                 EN: "CITY"
             },
             TEXT8:{
+                ES: "<label class='tracking-wide text-xs font-bold mb-2' for='hab'>COMENTARIO</label><div><textarea class='w-full bg-gray-200 border border-gray-200 px-4 py-2 mb-2 resize-none' id='comment' type='text' placeholder='Déjanos un comentario'></textarea></div>",
                 EN: "<label class='tracking-wide text-xs font-bold mb-2' for='hab'>COMMENTARY</label><div><textarea class='w-full bg-gray-200 border border-gray-200 px-4 py-2 mb-2 resize-none' id='comment' type='text' placeholder='Leave us some comment'></textarea></div>"
             },
             TEXT9:{
+                ES: "ENVIAR",
                 EN: "SEND"
             }
         }
     },
     FOOTER: {
         TEXT1: {
-            EN: "PRIVACY"
+            ES: "Privacidad",
+            EN: "Privacy"
         }
     }
 }
